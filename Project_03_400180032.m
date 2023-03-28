5fca8cb177faf8222ec3de41bd496889e121ad6c
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-03-28 11:44:44 -0400

    modified if statments

--- Project_03_400180032.m
+++ Project_03_400180032.m
@@ -6,12 +6,12 @@ clc; clear;
 % load full colour image
 training_img = im2double(imread('training.jpg'));
 
-% Extract the color channels corresponding to the Bayer pattern
+% Extract the colour channels corresponding to the Bayer pattern
 r = training_img(:,:,1);
 g = training_img(:,:,2);
 b = training_img(:,:,3);
 
-% Convert seperate channels to individual column vectors
+% Convert separate channels to individual column vectors
 r_col = im2col(r, [1,1]);
 g_col = im2col(g, [1,1]);
 b_col = im2col(b, [1,1]);
@@ -21,13 +21,13 @@ r_window = im2col(padarray(r,[2,2],'symmetric', 'both'),[5 5]);
 g_window = im2col(padarray(g,[2,2],'symmetric', 'both'),[5 5]);
 b_window = im2col(padarray(b,[2,2],'symmetric', 'both'),[5 5]);
 
-% Calculate coefficents
-[g_rggb,b_rggb] = rggb_coefficents(r_window,g_window,b_window,g_col,b_col); 
-[r_grbg,b_grbg] = grbg_coefficents(r_window,g_window,b_window,r_col,b_col);
-[r_gbrg,b_gbrg] = gbrg_coefficents(r_window,g_window,b_window,r_col,b_col);
-[r_bggr,g_bggr] = bggr_coefficents(r_window,g_window,b_window,r_col,g_col);
+% Calculate coefficients
+[g_rggb,b_rggb] = rggb_coefficients(r_window,g_window,b_window,g_col,b_col); 
+[r_grbg,b_grbg] = grbg_coefficients(r_window,g_window,b_window,r_col,b_col);
+[r_gbrg,b_gbrg] = gbrg_coefficients(r_window,g_window,b_window,r_col,b_col);
+[r_bggr,g_bggr] = bggr_coefficients(r_window,g_window,b_window,r_col,g_col);
 
-% ---- Perform Linear Regresion ----
+% ---- Process a test image ---
 input_img = im2double(imread('testing.png'));
 r = input_img(:,:,1);
 g = input_img(:,:,2);
@@ -35,8 +35,12 @@ b = input_img(:,:,3);
 [m, n, ~] = size(input_img);
 
 % Can change function for a different patch pattern -- this function is
-% used for testing purposes (creating a beyer image)
+% used for testing purposes (creating a bayer image)
 pattern_img = im2double(generate_grbg_patch(input_img, r, g, b));
+
+% use this if input image is already in bayer pattern format
+% pattern_img = im2double(input_img); 
+
 r_channel = pattern_img(:,:,1);
 g_channel = pattern_img(:,:,2);
 b_channel = pattern_img(:,:,3);
@@ -53,28 +57,32 @@ bw_padded(:,3) = [];
 bw_padded(end - 2,:) = [];
 bw_padded(:,end - 2) = [];
 
-
 % used to test for which bayer pattern is being used
 b_padded = padarray(b_channel,[3 3],'symmetric');
-b_padded(end-2,:) = []; b_padded(:,end-2) = [];
-b_padded(3,:) = []; b_padded(:,3) = [];
+b_padded(3,:) = []; 
+b_padded(:,3) = [];
+b_padded(end-2,:) = []; 
+b_padded(:,end-2) = [];
 
-% Process image
 for i=3:m+2
     for j=3:n+2
+        bayer_pattern = b_padded(i:i+1, j:j+1);
         window = im2double(bw_padded(i-2:i+2,j-2:j+2));
         window = window(:);
-        bayer_pattern = b_padded(i:i+1, j:j+1);
-        if isequal(bayer_pattern .* [1 1; 1 0], zeros(2))
+        % rggb
+        if (bayer_pattern(1,1) == 0 && bayer_pattern(1,2) == 0 && bayer_pattern(2,1) == 0)
             b_channel(i-2,j-2) = sum(b_rggb.*window);
             g_channel(i-2,j-2)= sum(g_rggb.*window);
-        elseif isequal(bayer_pattern .* [1 0; 1 1], zeros(2))
+        % gbrg
+        elseif (bayer_pattern(1,1) == 0 && bayer_pattern(2,1) == 0 && bayer_pattern(2,2) == 0)
             r_channel(i-2,j-2) = sum(r_gbrg.*window);
             b_channel(i-2,j-2)= sum(b_gbrg.*window);
-        elseif isequal(bayer_pattern .* [1 1; 0 1], zeros(2))
+        % grbg
+        elseif (bayer_pattern(1,1) == 0 && bayer_pattern(1,2) == 0 && bayer_pattern(2,2) == 0)
             b_channel(i-2,j-2)= sum(b_grbg.*window);
             r_channel(i-2,j-2)= sum(r_grbg.*window);
-        elseif isequal(bayer_pattern .* [0 1; 1 1], zeros(2))
+        % bggr
+        elseif (bayer_pattern(1,2) == 0 && bayer_pattern(2,1) == 0 && bayer_pattern(2,2) == 0)
             g_channel(i-2,j-2) = sum(g_bggr.*window);
             r_channel(i-2,j-2)= sum(r_bggr.*window);
         end
@@ -158,76 +166,72 @@ end
 
 
 % Generate coefficents for various patch patterns
-function [g_rggb,b_rggb] = rggb_coefficents(r_window,g_window,b_window,g_col,b_col)
+function [g_rggb,b_rggb] = rggb_coefficients(r_window,g_window,b_window,g_col,b_col)
     % rggb matrix -> [r g;g b]
     r_rep = repmat ([1 0;0 0], [3,3]);
     g_rep = repmat ([0 1;1 0], [3,3]);
     b_rep = repmat ([0 0;0 1], [3,3]);
 
-    % Apply offset to ensure center of the pixel is aligned with the 
-    % corresponding pixel in the image
+    % Remove the final column and row to create a 5x5 window
     r_rep(6,:) = []; r_rep(:,6) = [];
     g_rep(6,:) = []; g_rep(:,6) = [];
     b_rep(6,:) = []; b_rep(:,6) = [];
     
     X = (r_rep(:).*r_window + g_rep(:).*g_window + b_rep(:).*b_window)';
     
-    g_rggb = inv(X'*X)*X'*(g_col');
-    b_rggb = inv(X'*X)*X'*(b_col');
+    g_rggb = (X'*X)\X'*(g_col');
+    b_rggb = (X'*X)\X'*(b_col');
 end
 
 
-function [r_grbg,b_grbg] = grbg_coefficents(r_window,g_window,b_window,r_col,b_col)
+function [r_grbg,b_grbg] = grbg_coefficients(r_window,g_window,b_window,r_col,b_col)
     % grbg matrix -> [g r;b g]
     r_rep = repmat ([0 1;0 0], [3,3]);
     g_rep = repmat ([1 0;0 1], [3,3]);
     b_rep = repmat ([0 0;1 0], [3,3]);
 
-    % Apply offset to ensure center of the pixel is aligned with the 
-    % corresponding pixel in the image
+    % Remove the final column and row to create a 5x5 window
     r_rep(6,:) = []; r_rep(:,6) = [];
     g_rep(6,:) = []; g_rep(:,6) = [];
     b_rep(6,:) = []; b_rep(:,6) = [];
     
     X = (r_rep(:).*r_window + g_rep(:).*g_window + b_rep(:).*b_window)';
     
-    r_grbg = inv(X'*X)*X'*(r_col');
-    b_grbg = inv(X'*X)*X'*(b_col');
+    r_grbg = (X'*X)\X'*(r_col');
+    b_grbg = (X'*X)\X'*(b_col');
 end
 
-function [r_gbrg,b_gbrg] = gbrg_coefficents(r_window,g_window,b_window,r_col,b_col)
+function [r_gbrg,b_gbrg] = gbrg_coefficients(r_window,g_window,b_window,r_col,b_col)
     % gbrg matrix -> [g b;r g]
     r_rep = repmat ([0 0;1 0], [3,3]);
     g_rep = repmat ([1 0;0 1], [3,3]);
     b_rep = repmat ([0 1;0 0], [3,3]);
 
-    % Apply offset to ensure center of the pixel is aligned with the 
-    % corresponding pixel in the image
+    % Remove the final column and row to create a 5x5 window
     r_rep(6,:) = []; r_rep(:,6) = [];
     g_rep(6,:) = []; g_rep(:,6) = [];
     b_rep(6,:) = []; b_rep(:,6) = [];
 
     X = (r_rep(:).*r_window + g_rep(:).*g_window + b_rep(:).*b_window)';
     
-    r_gbrg = inv(X'*X)*X'*(r_col');
-    b_gbrg = inv(X'*X)*X'*(b_col');
+    r_gbrg = (X'*X)\X'*(r_col');
+    b_gbrg = (X'*X)\X'*(b_col');
 end
 
-function [r_bggr,g_bggr] = bggr_coefficents(r_window,g_window,b_window,r_col,g_col)
+function [r_bggr,g_bggr] = bggr_coefficients(r_window,g_window,b_window,r_col,g_col)
     % bggr matrix -> [b g;g r]
     r_rep = repmat ([0 0;0 1], [3,3]);
     g_rep = repmat ([0 1;1 0], [3,3]);
     b_rep = repmat ([1 0;0 0], [3,3]);
 
-    % Remove the last row and column in the array to create a 5x5 matrix
-    % as shown in the lab instructions
+    % Remove the final column and row to create a 5x5 window
     r_rep(6,:) = []; r_rep(:,6) = [];
     g_rep(6,:) = []; g_rep(:,6) = [];
     b_rep(6,:) = []; b_rep(:,6) = [];
     
     X = (r_rep(:).*r_window + g_rep(:).*g_window + b_rep(:).*b_window)';
     
-    r_bggr = inv(X'*X)*X'*(r_col');
-    g_bggr = inv(X'*X)*X'*(g_col');
+    r_bggr = (X'*X)\X'*(r_col');
+    g_bggr = (X'*X)\X'*(g_col');
 end
 
